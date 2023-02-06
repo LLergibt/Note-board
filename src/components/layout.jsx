bb7976ab3a8f4fdb9335d31811b9c42c9c27917e
d1mnaz <dimonnaz@gmail.com>
2023-02-07 00:31:00 +0300

    add grid layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './layout.css'
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-
 const Layout = () => {
   const [hidden, setHidden] = useState(false);
   const onClick = (e) => {
@@ -11,32 +9,32 @@ const Layout = () => {
       return !prev
     })
   }
-  console.log(hidden)
   return (
-   <>
-    <div className="layout">
-    <div className={hidden? '' :  "sidebar"}>
-      <button className={hidden ? "hidden-sidebar-button": "sidebar-button"} onClick={onClick}>
-        {hidden? <i class="gg-chevron-double-right-r"></i>: <i class="gg-chevron-double-left-r"></i>}
-      </button>
-    </div>
-    <div className="header">
-      <h1>
-        Askarate.moscow
-      </h1>
-      <div className="share">
-      <button>
-          Поделиться
+    <div className="container">
+      <div className={hidden ? 'sidebar-hidden' : "sidebar"}>sidebar
+        <button onClick={onClick}>
+          focalboard
         </button>
+
+        <ul>
+          <li>
+            fdhvfi
+          </li>
+          <li>
+            dsfsdojf
+          </li>
+          <li>
+            dfdsfsd
+          </li>
+          <li>
+            dfdsfsd
+          </li>
+        </ul>
       </div>
-    </div>
-    </div>
+      <div className="header">header</div>
+      <div className="content">content</div>
 
-    <div className="outlet">
-      <Outlet/>
     </div>
-
-   </>
   )
 }
 export default Layout
