5b7b34d516f820e1b2bed5261b380fb2894ccc83
GGergibt <ggergbit@naz.gmail.com>
2023-03-08 02:49:42 +0300

    Добавил cors в api

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import createTables from './models/modelsCreate.js'
 import {notes} from './api/notes.js'
 import {auth} from './api/auth.js'
+import {board} from './api/board.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 await createTables()
 
@@ -13,6 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/notes', notes)
 app.use('/auth', auth)
+app.use('/board', board)
 
 app.get('/', (req, res) => {
 	res.send("<h1>OK</h1>")
