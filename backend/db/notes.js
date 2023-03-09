adbeb8d30a5357f4363b7afd13204b438735bf6f
GGergibt <ggergbit@naz.gmail.com>
2023-03-10 01:56:02 +0300

    Добавил название свойства в запрос

--- backend/db/notes.js
+++ backend/db/notes.js
@@ -10,6 +10,7 @@ export const propertyCreate = async (body) => {
   return property
 }
 export const notesGet = async (boardId) => {
-  const [notesRaw, metadata] = await sequelize.query(`select title,  ARRAY_AGG(json_build_object('property_data', property_data, 'property_id', property_id)) as properties  from note where board_id = ${boardId}  group by title`)
+  const [notesRaw, metadata] = await sequelize.query(`SELECT n.title,  ARRAY_AGG(json_build_object('property_title', pt.title, 'property_data', property_data, 'property_id', property_id)) as properties  FROM note n JOIN property_title pt ON pt.id = n.property_id WHERE n.board_id = ${boardId}  GROUP BY n.title
+`)
   return notesRaw
 }
