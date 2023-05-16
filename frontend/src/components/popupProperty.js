b849aa11683fd019ead276549590994fcc5a14e0
GGergibt <ggergbit@naz.gmail.com>
2023-05-17 01:43:18 +0300

    Изменил хук, добавил функционал изменения названия свойства. Получаю обработчик событий через хук

--- frontend/src/components/popupProperty.js
+++ frontend/src/components/popupProperty.js
@@ -2,18 +2,20 @@ import React from 'react';
 import 'css/popup-note.css';
 import {useRef} from 'react'
 import PropertyItem from 'components/propertyItem'
+import {useHandleNote} from 'hooks/useHandleNote'
 
-const PopupProperty = ({property, onSubmit}) => {
+const PopupProperty = ({property}) => {
        const title = useRef('')
        const data = useRef('')
+       const {onChangeNote} = useHandleNote()
 
         return (
           <div className="property">
-            <form onSubmit={event => onSubmit(event, {name: 'property_title', ref: title})}>
+            <form onSubmit={event => onChangeNote(event, {id: property.property_id, title: title.current.value}, 'property_title')}>
             <input ref={title} defaultValue={property.property_title} className="note-property" >
             </input>
           </form>
-            <form onSubmit={event => onSubmit(event, {name: 'property_data', ref: data})}>
+            <form onSubmit={event => onChangeNote(event, {id: property.property_id, data: data.current.value}, 'property_data')}>
           <input ref={data} defaultValue={property.property_data} className="note-property" placeholder="пустой">
           </input>
           </form>
--- frontend/src/hooks/useHandleNote.js
+++ frontend/src/hooks/useHandleNote.js
@@ -1,23 +1,33 @@
 import { useEffect, useState, useRef, useContext } from 'react';
 import {NoteContext} from 'components/layout'
+import {RefreshContext} from 'components/noteItems'
+import axios from 'axios'
 
 export const useHandleNote = () => {
   const {note, setNote} = useContext(NoteContext)
+  const onReload = useContext(RefreshContext)
 
-  const changeNote = (items) => {
+  const changePropertyTitle = (data) => axios.post('notes/property/change/title', data).then(() => onReload())
 
-  }
-  const onSubmit = (event, property) => {
-   event.preventDefault()
-   console.log(property.ref.current.value)
-   console.log(property.name)
+  const onChangeNote = (event, property, expression) => {
+    event.preventDefault()
+    switch(expression) {
+      case 'property_title':
+        changePropertyTitle(property)
+        break
+      case 'property_data':
+        console.log('data', property.id, property.data)
+        break
+      default:
+        console.log('')
+
+    }
   }
   const addNoteInContext = noteItem => setNote(noteItem)
 
   return {
     note,
-    onSubmit,
-    changeNote,
+    onChangeNote,
     addNoteInContext
   }
 
