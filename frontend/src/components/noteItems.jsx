2dd82297ccd39c799d14deb0f3a956c44a726322
GGergibt <ggergbit@naz.gmail.com>
2023-05-17 01:36:11 +0300

    Сделал контекст с функцией, которая заставляет компонент запросить данные о заметках еще раз

--- frontend/src/components/noteItems.jsx
+++ frontend/src/components/noteItems.jsx
@@ -1,17 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, createContext} from 'react';
 import 'css/itemNote.css'
 import NoteItem from 'components/noteItem'
 import axios from 'axios'
 
+export const RefreshContext = createContext()
 const NoteItems = () => {
   const [notes, setNotes] = useState()
+  const [reloadDataAfterPostReq, set] = useState(false)
+  const onReload = () => set(!reloadDataAfterPostReq)
   useEffect(() => {
     axios.get('/notes/?board=1').then((response) => {setNotes(response.data)})
-  }, [])
+
+  }, [reloadDataAfterPostReq])
   return (
     <>
+    <RefreshContext.Provider value={onReload}>
     {notes && notes?.map((note) => 
       <NoteItem key={note.id} note={note}/>)}
+    </RefreshContext.Provider>
     </>
     
   )
