b6d6a7479cf41281f0d98d9a002ad921988e5275
GGergibt <ggergbit@naz.gmail.com>
2023-05-17 00:04:48 +0300

    Добавил роут с изменением названия свойства. Изменил названия полей в json модели свойств

--- backend/api/notes.js
+++ backend/api/notes.js
@@ -1,18 +1,24 @@
 import express from 'express'
-import {noteCreate, propertyCreate, notesGet, propertyNoteCreate} from '../db/notes.js'
+import {noteCreate, propertyCreate, notesGet, propertyNoteCreate, propertyTitleChange} from '../db/notes.js'
 
 export const notes = express.Router();
 
 notes.post('/', async function(req, res) {
-  await noteCreate(req.body)
+  const note = await noteCreate(req.body)
   res.status(201)
-  res.send('succeed')
+  res.json(note)
 })
 
 notes.post('/property-note', async function(req, res) {
   await propertyNoteCreate(req.body)
   res.status(201)
   res.send('succeed')
+})
+notes.post('/property/change/title', async function(req, res) {
+  await propertyTitleChange(req.body)
+  res.status(201)
+  res.send('succeed')
+
 })
 
 notes.get('/', async function(req, res) {
@@ -21,7 +27,7 @@ notes.get('/', async function(req, res) {
   res.json(notes)
 })
 
-notes.post('/property-title', async function(req, res) {
+notes.post('/property', async function(req, res) {
   await propertyCreate(req.body)
   res.status(201)
   res.send('succeed')
--- backend/db/notes.js
+++ backend/db/notes.js
@@ -9,19 +9,20 @@ export const noteCreate = async (body) => {
     PropertyNote.create({note_id: note.id, property_id: property.id, property_data: ''})
   })
 
-
   return note
 }
 export const propertyNoteCreate = async (body) => {
   const propertyNote = await PropertyNote.create(body)
   return propertyNote
 }
+export const propertyTitleChange = async (body) => await sequelize.query(`UPDATE property SET title='${body.title}' WHERE id=${body.id}`)
+
 export const propertyCreate = async (body) => {
   const property = await PropertyTitle.create(body)
   return property
 }
 export const notesGet = async (boardId) => {
-  const [notesRaw, metadata] = await sequelize.query(`SELECT n.id, n.title, ARRAY_AGG(json_build_object('property_title', p.property_title, 'property_data', pn.property_data, 'property_id', pn.property_id)) as properties  FROM note n JOIN property_note pn ON n.id = pn.note_id JOIN  property p ON p.id = pn.property_id WHERE n.board_id = ${boardId} GROUP BY n.id
+  const [notesRaw, metadata] = await sequelize.query(`SELECT n.id, n.title, ARRAY_AGG(json_build_object('property_title', p.title, 'property_data', pn.property_data, 'property_id', pn.property_id)) as properties  FROM note n JOIN property_note pn ON n.id = pn.note_id JOIN  property p ON p.id = pn.property_id WHERE n.board_id = ${boardId} GROUP BY n.id
 `)
   return notesRaw
 }
