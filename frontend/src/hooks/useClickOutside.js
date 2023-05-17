e5a670623c193291f77600058e96d51b78db0739
GGergibt <ggergbit@naz.gmail.com>
2023-05-18 00:52:15 +0300

    Сделал перезагрузку данных при закрытии попапа. Это необходимо для того чтобы отобразить созданую заметку даже если поля в ней не изменялись. Подобный функционалъ

--- frontend/src/hooks/useClickOutside.js
+++ frontend/src/hooks/useClickOutside.js
@@ -1,8 +1,12 @@
-import { useEffect} from 'react';
+import { useEffect, useContext} from 'react';
+import {RefreshContext} from 'components/layout'
+
 export const useClickOutside = (onClickOutside, ref) => {
+  const onReload = useContext(RefreshContext)
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
+        onReload && onReload()
         onClickOutside && onClickOutside();
 
       }
