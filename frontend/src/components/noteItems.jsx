2d31e8c8313d55f34607e2fd680f8a0727ab20c3
LLergibt <gosha2007naz@gmail.com>
2023-07-02 17:30:25 +0300

    добавил проверку перед тем как слать запрос

--- frontend/src/components/noteItems.jsx
+++ frontend/src/components/noteItems.jsx
@@ -8,13 +8,15 @@ const NoteItems = () => {
   const [properties, setProperties] = useState()
   const reloadDataAfterPostReq = useContext(RefreshContext)
   useEffect(() => {
+    if (!notes) {
     axios.get('/notes/?board=1').then(response => setNotes(response.data))
     axios.get('/notes/properties/?board_id=1').then(response => setProperties(response.data))
+    }
 
   }, [reloadDataAfterPostReq])
   return (
     <>
-    {notes && notes?.map((note) => 
+    {notes && notes.map((note) => 
       <NoteItem key={note.id} note={note} properties={properties}/>)}
     </>
     
