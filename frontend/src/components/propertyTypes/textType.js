3feedb223e1a7caea2188397e4ad35dab3e1e290
LLergibt <gosha2007naz@gmail.com>
2023-07-02 16:23:16 +0300

    Добавил onSubmitHandler

--- frontend/src/components/propertyTypes/textType.js
+++ frontend/src/components/propertyTypes/textType.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {useRef} from 'react'
 import {useHandleNote} from 'hooks/useHandleNote'
 
-const TextType = (property) => {
+const TextType = ({ property }) => {
   const data = useRef()
+  const { onChangeNote } = useHandleNote()
 
   return (
-             <input ref={data} defaultValue={property.data} placeholder="пустой">
-           </input>
+    <form onSubmit={(event) => {onChangeNote(event, {id: property.id, data: data.current.value}, 'property_data')}}>
+       <input  ref={data} defaultValue={property.data} placeholder="пустой">
+       </input>
+   </form>
   )
 
 }
