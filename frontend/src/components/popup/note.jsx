cec56f256f42b34e3111bee819c8b26e436257e1
LLergibt <gosha2007naz@gmail.com>
2023-07-02 15:36:41 +0300

    Убрал лог, который использовался при отладке

--- frontend/src/components/popup/note.jsx
+++ frontend/src/components/popup/note.jsx
@@ -8,7 +8,6 @@ const Note = ({onClickOutside}) => {
   const title = useRef('')
   useClickOutside(onClickOutside, ref)
   const {note, properties, onChangeNote, addProperty} = useHandleNote()
-  console.log(properties)
   //<div className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
   //   <div ref={ref} className="relative w-full max-w-2xl max-h-full">
   //     </input>
