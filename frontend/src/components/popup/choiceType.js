4fe4f0eb8f05f85193c1d3a44135bd917f2f99d9
LLergibt <gosha2007naz@gmail.com>
2023-07-02 16:05:28 +0300

    Сделал тоже самое, что и в прошлов коммите, только для попапа с добавлением варианта при категории choice

--- frontend/src/components/popup/choiceType.js
+++ frontend/src/components/popup/choiceType.js
@@ -31,13 +31,13 @@ const ChoiceTypePopup = ({onClickOutside}) => {
 
   useClickOutside(onClickOutside, ref)
   return (
-    <div ref={ref} className="">
+    <div ref={ref} className="p-0 pl-6  m-0">
     <form onSubmit={(event) => onChangeNote(event, {title: option, property_id: property.property_id, property_note_id: property.id}, 'choice')}>
     <input ref={inputReference} autoFocus onChange={handleChange} className={option? '': ""}>
     </input>
     </form>
-    <button className={option ? "z-10 relative bottom-6 opacity-0" : "z-10 relative opacity-25 bottom-6"} onClick={changePopupByCall}>
-       Пустой
+    <button className={option ? "z-10 absolute bottom-6 m-0 p-0 opacity-0 top-20 my-1" : "absolute opacity-25 top-20 my-1"} onClick={changePopupByCall}>
+       пустой
     </button>
 
 
--- frontend/src/components/popup/selectChoiceType.js
+++ frontend/src/components/popup/selectChoiceType.js
@@ -6,7 +6,7 @@ const SelectChoiceType = ({newOption}) => {
   //const {onChangeNote, onDeleteProperty} = useHandleNote()
 
   return (
-    <div className="absolute z-100">
+    <div className="absolute z-100 my-5">
     <div className="shadow-2xl w-64 h-3/4  bg-white border rounded-sm  font-extralight text-base ">
       <p className="text-center py-3 pl-1 text-gray-400 ">
         {newOption ? newOption : 'Нет вариантов. Начните печатать, чтобы добавить новый'}
