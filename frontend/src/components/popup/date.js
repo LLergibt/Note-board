7f40b68132db8bbb0640357fe7c1a439ee0abecb
LLergibt <gosha2007naz@gmail.com>
2023-08-10 15:42:04 +0300

    Исправил ошибку с неправильным типо данных

--- frontend/src/components/popup/date.js
+++ frontend/src/components/popup/date.js
@@ -10,9 +10,9 @@ const DatePopup = ({ property, onClickOutside }) => {
   const ref = useRef()
   const {onChangeNote} = useHandleNote()
   useClickOutside(onClickOutside, ref)
-  const [date, setDate] = useState(new Date(property.data) ?? new Date());
+  const [date, setDate] = useState(property.data ? new Date(property.data) : new Date());
   const handleDate = (newDate) => {
-      setDate(new String(newDate))
+      setDate(new Date(newDate))
       onChangeNote({preventDefault: () => {}}, {id: property.id, data: newDate.toLocaleDateString("fr-CA", {year:"numeric", month: "2-digit", day:"2-digit"})}, 'property_data')
   }
 
